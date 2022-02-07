8cbe7532c9df3b406f1bc84ad4c5940affc1c351
Caleb Finley <31387217+KazeIU@users.noreply.github.com>
2022-02-07 14:59:57 +0900

    test github actions

--- .github/scripts/export_ipa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -archivePath $PWD/build/Calculator.xcarchive \
-            -exportOptionsPlist Calculator-iOS/Calculator\ iOS/exportOptions.plist \
-            -exportPath $PWD/build \
-            -allowProvisioningUpdates \
-            -exportArchive | xcpretty
--- .github/scripts/publish_testflight.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcrun altool --upload-app -t ios -f build/Calculator\ iOS.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
